971c39953bcb161ccf767580dd3ed20ffc942a61
Erremme <Erremme2.0@gmail.com>
2025-05-20 17:51:38 +0200

    feat: added validation

--- src/App.jsx
+++ src/App.jsx
@@ -9,27 +9,59 @@ const initialFormValue = {
  description : ""
 }
 
+const initialErrors = {
+  firstName: false,
+  lastName: false,
+  password: false,
+  specialization: false,
+  experienceYears: false,
+  description: false
+}
+
 export default function App() {
   const [form ,setForm] = useState(initialFormValue)
+  const [errors , setErrors] = useState(initialErrors)
+
+  
 
   const handleChange = (e) =>  {
     const fieldName = e.target.name;
     const value = e.target.value
-   setForm((current) => {
-    return{
+
+      setForm((current) => {
+          return{
       ...current,
       [fieldName] : value
+         }
+      }
+   )
+
+   setErrors((current) => {
+    return{
+      ...current,
+      [fieldName] : value.trim() === "" || fieldName === "experienceYears" && value <= 0
     }
-   } )
-   
+   })
   }
 
-  console.log(form)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(Object.values(errors).some((error) => error !== false )){
+       return;
+    }else{
+      console.log(form)
+    }
+
+  }
+  
+
+
+  
   return(
     <div className="container">
       <h1>Signup</h1>
 
-      <form>
+      <form onSubmit={handleSubmit} >
 
         <div>
           <label htmlFor="firstName">Nome</label>
@@ -40,7 +72,11 @@ export default function App() {
            value={form.firstName}
            onChange={handleChange}
            
+           
            />
+           {errors.firstName && 
+            <p className="errors">Compila il campo</p>
+           }
           
         </div>
 
@@ -52,8 +88,11 @@ export default function App() {
           placeholder="Inserisci il Cognome"
           value={form.lastName}
            onChange={handleChange}
-          
           />
+
+          {errors.lastName && 
+            <p className="errors">Compila il campo</p>
+           }
         </div>
 
         <div>
@@ -65,8 +104,12 @@ export default function App() {
            value={form.password}
            onChange={handleChange}
            />
-        </div>
 
+           {errors.password && 
+            <p className="errors">Compila il campo</p>
+           }
+        </div>
+         
          <select name="specialization" onChange={handleChange} >
             <option value="">Scegli la specializzazione</option>
             <option value="Full Stack">Full Stack</option>
@@ -74,17 +117,26 @@ export default function App() {
             <option value="Backend">Backend</option>
          </select>
 
+         {errors.specialization && 
+            <p className="errors">Compila il campo</p>
+           }
+
+
+
         <div>
           <label htmlFor="experienceYears">Anni di esperienza</label>
           <input
            type="number"
+           min={1}
             name="experienceYears"
             placeholder="Inscerisci quanti anni di esperienza hai"
             value={form.experienceYears}
            onChange={handleChange}
-             
-           
              />
+
+             {errors.experienceYears && 
+            <p className="errors">Il numero deve essere maggiore di "0"</p>
+           }
         </div>
         <div>
           <label htmlFor="description">Descrizione</label>
@@ -93,10 +145,15 @@ export default function App() {
              placeholder="Inserisci una breve descrizione  su di te"
              value={form.description}
             onChange={handleChange}
-             
-             
-             ></textarea>
+             >
+
+             </textarea>
+
+             {errors.description && 
+            <p className="errors">Compila il campo</p>
+           }
         </div>
+        <button  type="submit"> invia</button>
       </form>
     </div>
   )
